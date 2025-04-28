26638c2beda2b98e002570f32779afd8327cd1ee
vtroitino <vtroitino@uade.edu.ar>
2025-04-28 19:30:29 -0300

    Mejero documentación de cesar

--- encriptado/cesar.py
+++ encriptado/cesar.py
@@ -1,22 +1,32 @@
 def ascii_imprimibles() -> str:
     """
-    devuelve la tabla de caracteres  ascii imprimibles  
+    Devuelve la tabla de caracteres  ascii imprimibles  
     """
     return "".join(chr(x) for x in range(32, 127))
 
+def cifrar_ascii(caracter, clave):
+    """
+    Cifra un caracter usando el alfabeto ascii imprimible.
+    Si el caracter no es un ascii imprimible, se devuelve el caracter sin cambios.
+    """
+    alfabeto = ascii_imprimibles()
+    if caracter in alfabeto:
+        nuevo_caracter = alfabeto[(alfabeto.index(caracter) + clave) % len(alfabeto)]
+        return nuevo_caracter
+    return caracter
+
 def cifrar_caracter(caracter, mayus, clave, alfabeto):
+    """
+    Cifra un caracter usando el cifrado César.
+    Si el caracter está en el alfabeto proporcionado, se cifra.
+    Si no, se devuelve el caracter sin cambios.
+    """
     caracter_mayus = caracter.upper()
     if caracter_mayus in alfabeto:
         nueva_letra = alfabeto[(alfabeto.index(caracter_mayus) + clave) % len(alfabeto)]
         return nueva_letra if mayus else nueva_letra.lower()
     return caracter
 
-def cifrar_ascii(caracter, clave):
-    alfabeto = ascii_imprimibles()
-    if caracter in alfabeto:
-        nuevo_caracter = alfabeto[(alfabeto.index(caracter) + clave) % len(alfabeto)]
-        return nuevo_caracter
-    return caracter
 
 def cesar(mensaje: str, clave: int, descifrar: bool = False, alfabeto: str = "") -> str:
     """
