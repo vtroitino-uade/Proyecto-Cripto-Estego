15bbc183fab8c0a894af495a0c690407f2ecb0c6
Valentin <vtroitino@uade.edu.ar>
2025-06-01 01:54:27 -0300

    Añade módulo de constantes y mejora la implementación de LSB Matching para codificación y decodificación de mensajes en imágenes.

---
+++ config/constantes.py
@@ -0,0 +1,7 @@
+"""
+Modulo de constantes del proyecto.
+"""
+
+BYTE = 8  # Cantidad de bits en un byte
+RGB = 3  # Cantidad de canales de color en una imagen RGB
+DELIMITADOR = "<FIN>"  # Delimitador para el final del mensaje oculto en la imagen
--- estego/lsb_matching.py
+++ estego/lsb_matching.py
@@ -3,32 +3,46 @@ Módulo de la tecnica LSB Matching para ocultar y extraer mensajes en imágenes.
 """
 
 from PIL import Image
+from config.constantes import BYTE, DELIMITADOR
 
-def codificar_lsb_matching(imagen: Image.Image, mensaje: str) -> Image.Image:
+def convertir_a_bits(mensaje_bytes: bytes) -> list[int]:
+    """
+    Convierte un mensaje de bytes a una lista de números enteros representando sus bits.
+
+    Args:
+        mensaje_bytes (bytes): El mensaje de bytes a convertir. 
+
+    Returns:
+        list[int]: Lista de bits representando la cadena de bytes.
+    """
+    return [int(bit) for byte in mensaje_bytes for bit in f"{byte:08b}"]
+
+def _codificar_lsb(imagen: Image.Image, mensaje_bytes: bytes) -> Image.Image:
     """
     Codifica un mensaje en una imagen utilizando la técnica LSB Matching.
 
     Args:
-        imagen (Image): La imagen en la que se ocultará el mensaje. Debe ser un objeto de la clase Image de PIL.
-        mensaje (str | None): El mensaje a ocultar en la imagen.
+        imagen (Image): La imagen en la que se ocultará el mensaje. \
+        Debe ser un objeto de la clase Image de PIL.
+        mensaje_bytes (bytes): El mensaje a ocultar en la imagen.
 
     Returns:
         Image: Devuelve la imagen con el mensaje oculto.
-
     """
 
-def decodificar_lsb_matching(imagen: Image.Image) -> str:
+def _decodificar_lsb(imagen: Image.Image) -> str:
     """
     Decodifica un mensaje oculto en una imagen utilizando la técnica LSB Matching.
 
     Args:
-        imagen (Image): La imagen en la que se extraerá el mensaje. Debe ser un objeto de la clase Image de PIL.
+        imagen (Image): La imagen en la que se extraerá el mensaje. \
+        Debe ser un objeto de la clase Image de PIL.
 
     Returns:
         str: Devuelve el mensaje extraído de la imagen.
     """
 
-def lsb_matching(imagen: Image.Image, mensaje: str | None = None) -> Image.Image | str:
+def lsb_matching(imagen: Image.Image, mensaje: str | None = None) -> str | Image.Image:
     """
     Oculta o extrae un mensaje en una imagen utilizando la técnica 
     LSB (Least Significant Bit) Matching.
@@ -37,16 +51,37 @@ def lsb_matching(imagen: Image.Image, mensaje: str | None = None) -> Image.Image
     - JPEG (.jpg, .jpeg)
     - PNG (.png)
     - BMP (.bmp)
-    - GIF (.gif)
     - PPM (.ppm)
 
     Args:
-        imagen (Image): La imagen en la que se ocultará o de la que se extraerá el mensaje. Debe ser un objeto de la clase Image de PIL.
-        mensaje (str | None): El mensaje a ocultar en la imagen. Si es None, se extraerá el mensaje de la imagen.
+        imagen (Image): La imagen en la que se ocultará o de la que se extraerá el mensaje. \
+        Debe ser un objeto de la clase Image de PIL.
+        mensaje (str | None): El mensaje a ocultar en la imagen. \
+        Si es None, se extraerá el mensaje de la imagen.
 
     Returns:
-        (Image | str): Si se proporciona un mensaje, devuelve la imagen con el mensaje oculto. Si no se proporciona un mensaje, devuelve el mensaje extraído de la imagen.
+        (str | Image): Si no se proporciona un mensaje, devuelve el mensaje extraído de la imagen. \
+        Si se proporciona un mensaje, devuelve la imagen con el mensaje oculto.
 
     Raises:
         ValueError: Si el mensaje es demasiado largo para ocultarlo en la imagen.
     """
+
+    if mensaje is None:
+        return _decodificar_lsb(imagen)
+
+    if DELIMITADOR in mensaje:
+        mensaje = mensaje.replace(DELIMITADOR, '')
+
+    ancho, alto = imagen.size
+    cantidad_canales = len(imagen.getbands())
+    longitud_maxima = (ancho * alto * cantidad_canales) // BYTE
+    mensaje = mensaje + DELIMITADOR
+    mensaje_bytes = mensaje.encode('utf-8')
+    longitud_mensaje = len(mensaje_bytes)
+
+    if longitud_mensaje > longitud_maxima:
+        raise ValueError(f"El mensaje debe ser menor o igual a {longitud_maxima} caracteres. \
+                         O la imagen es demasiado pequeña para ocultar el mensaje.")
+
+    return _codificar_lsb(imagen, mensaje_bytes)
