8fff2d810f311676c147b36a86e0c4bbbe15e726
vtroitino-uade <vtroitino@uade.edu.ar>
2025-06-23 14:39:34 -0300

    Refactor import statements in encriptado module and update feistel tests for consistency

--- encriptado/__init__.py
+++ encriptado/__init__.py
@@ -1,13 +1,5 @@
 from .cesar import cesar
 from .vigenere import vigenere
 from .xor import xor
-from .feistel import (
-    texto_a_binario,
-    binario_a_texto,
-    xor_feistel,
-    ronda_feistel,
-    feistel_cifra_binarios,
-    feistel_descifra_binarios,
-    generar_keys
-)
+from . import feistel
 from .hill import hill, validar_clave
